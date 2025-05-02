ca0a24fa575d16b2cc65671f81bb047b54c3618b
basaraannmustafa <basaranmustafa2005@gmail.com>
2025-05-02 04:32:47 +0300

    Hata giderildi

--- redis_listele.py
+++ redis_listele.py
@@ -1,24 +1,20 @@
-# redis_listele.py
 from redis_baglanti import redis_baglan
 
 def tum_kelimeleri_getir():
     r = redis_baglan()
     kelimeler = {}
 
-    for key in r.keys("*"):
-        if r.type(key) != b'hash':
+    for key in r.keys('*'):
+        if r.type(key) != 'hash':
             continue
 
         veri = r.hgetall(key)
-        anlam = veri.get("anlam", "")
-        es_anlam = veri.get("es_anlamlar", "")
-        orijinal = veri.get("orijinal", key)
-
-        # normalize edilmiş key ile saklıyoruz
-        kelimeler[key.lower()] = {
+        anlam = veri.get('anlam', '-')
+        es_anlam = veri.get('es_anlamlar', '')
+        orijinal = veri.get('orijinal', key)
+        kelimeler[orijinal] = {
             "anlam": anlam,
-            "es_anlamlar": es_anlam,
-            "orijinal": orijinal
+            "es_anlamlar": es_anlam
         }
 
     return dict(sorted(kelimeler.items()))
